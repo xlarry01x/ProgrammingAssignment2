5187d274eaa09e6141be07930f361b4fa35a2a48
xlarry01x <xlarry01x@users.noreply.github.com>
2016-06-25 00:31:24 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Catching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## This function creates a special matrix that cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix<-matrix(1:9, ncol = 3)
 
-}
+makeCacheMatrix
+
+        
+     
+## This function computes the inverse of the special matrix
+## returned by makeCacheMatrix.If the inverse has already
+## been calculated(and the matrix has not changed)
+## then the cachesolve should retrieve the inverse from
+## the cache.       
+ 
 
 
-## Write a short comment describing this function
+cacheSolve <-svd(makeCacheMatrix)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve
+
+cacheSolve<-chol (makeCacheMatrix)
+
+cacheSolve
+                         
+
+## Use of any 2 solve functions i.e svd() or chol()
+
+
+
+
+
+
+
+
+
